b72ece2141aaa1f4a7d23ec15d4c909e380d6e9e
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-12 17:54:03 +0600

    es6 done

--- es6.js
+++ es6.js
@@ -11,3 +11,26 @@ ${student.movies[1]}`
 console.log(about);
 
 // arrow function
+const getFiftyfive = () => 55;
+const addsixtyFive = num => num + 65;
+const isEven = x => x % 2 == 0;
+// multy arrow function 
+const addThree = (x, y, z) => x + y + z;
+// multy line arrow function
+const doMath = (num1, num2) => {
+    const sum = num1 + num2;
+    return sum;
+}
+const result = doMath(5, 5);
+console.log(result);
+// spread operator
+
+const newNumber = [...numbers]
+numbers.push(99);
+console.log(numbers);
+
+console.log(newNumber);
+
+// create a new array from a older array and add an element
+const currentNumber = [...numbers, 55]
+console.log(currentNumber);
