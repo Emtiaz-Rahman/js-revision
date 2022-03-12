b73a7a19def77e7eda7ffaf2096a5645dadab556
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-12 11:02:21 +0600

    templete string

---
+++ es6.js
@@ -0,0 +1,13 @@
+// template string
+const numbers = [45, 52, 48, 52];
+
+const student = {
+    name: 'salam',
+    age: 45,
+    movies: ['king', 'badsha']
+};
+const about = `My name is ${student.name} age of ${student.age} also liked movie 
+${student.movies[1]}`
+console.log(about);
+
+// arrow function
