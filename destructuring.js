c3aa9d52a6307c4fd7764185db5c0955ed14988a
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-13 13:30:08 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+// array destructuring
+const numbers=[40,65];
+// const [x,y]=[42,45];
+
+
+const [x,y]=numbers;
+console.log(x,y);
+
+function boxiFy(num1,num2){
+    const nums=[num1,num2];
+    return nums;
+}
+console.log(boxiFy(90,45));
